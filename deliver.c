3aea7c5403366dbb0bb5f419a803dbdb3a2715d6
Edward9292 <58920613+Edward9292@users.noreply.github.com>
2023-02-28 15:24:50 -0500

    lab3 update

--- deliver.c
+++ deliver.c
@@ -82,15 +82,27 @@ void send_file(char* filename, int socket_desc, struct sockaddr_in server_addr){
         printf("bytes: %d\n", bytes);
 
         int num;
-        if((num = recvfrom(socket_desc, server_message, sizeof(server_message), 0,
+        int resent_count = 0;
+        int max_resent = 10;
+        while((num = recvfrom(socket_desc, server_message, sizeof(server_message), 0,
             (struct sockaddr*)&server_addr, &server_struct_length)) < 0){
-            printf("Error while receiving server's msg\n");
-            exit(1);
+            printf("Timeout or Error when trying to  received packet from server. frag_no: %d\n", i);
+            printf("Re-sending the packet....\n");
+            if ((bytes = sendto(socket_desc, message, strlen(message), 0, (struct sockaddr *) &server_addr, sizeof(server_addr))) <= 0) {
+                printf("Error while sending server msg\n");
+                exit(1);
+            }
+            resent_count++;
+            if(resent_count>max_resent){
+                printf("Loss connection with server!\n");
+                exit(1);
+            }
+            
         }
         printf("%d\n",num);
         clock_t end = clock();
 
-
+        //update time out
         clock_t curr_RTT = end - start;
         RTT_estimation = 0.875*RTT_estimation + ((double)curr_RTT/8);
         RTT_deviation = 0.75*RTT_deviation + 0.25*abs(RTT_estimation-curr_RTT);
